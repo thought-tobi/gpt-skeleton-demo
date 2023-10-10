fbb959107f49a01b8b4c6774a2b72c3cd1851484
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-10-10 15:32:54 +0200

    add literal translation for parallelization demo

---
+++ src/literal_translations.py
@@ -0,0 +1,70 @@
+import concurrent
+import json
+from dataclasses import dataclass, field
+
+import dacite
+
+from src.util.decorators import timed
+from src.util.message import Message, USER, SYSTEM
+from util.client import exchange
+import logging
+
+SYSTEM_PROMPT = """Provide literal translations for words in the context of a sentence.
+You will receive a JSON with a sentence and one or multiple words, and provide a response in the following structure:
+[
+    {
+      "word": "PLACEHOLDER_WORD",
+      "translation": "PLACEHOLDER_LITERAL_TRANSLATION"
+    },
+]
+"""
+
+
+@dataclass
+class Word:
+    word: str
+    translation: str
+
+
+@dataclass
+class LiteralTranslation:
+    words: list[Word] = field(default_factory=list)
+
+
+@timed
+def get_literal_translation(sentence: str, chunk_size: int = 2) -> LiteralTranslation:
+    chunks = chunk_sentence(sentence, chunk_size)
+    literal_translation = LiteralTranslation()
+    # Create a ThreadPoolExecutor to process chunks concurrently
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(generate_literal_translation_for_chunk, sentence, chunk) for chunk in chunks]
+
+        for future in concurrent.futures.as_completed(futures):
+            translation = future.result()
+            for word in translation:
+                literal_translation.words.append(word)
+
+    return literal_translation
+
+
+def chunk_sentence(sentence: str, chunk_size) -> list[list[str]]:
+    sentence = list(sentence.split(' '))
+    chunks = []
+    for i in range(0, len(sentence), chunk_size):
+        # Get a chunk of the sentence and append it to the list of chunks
+        chunk = sentence[i:i + chunk_size]
+        chunks.append(chunk)
+    return chunks
+
+
+def generate_literal_translation_for_chunk(sentence: str, chunk: list[str]) -> list[Word]:
+    messages = [Message(role=SYSTEM, content=SYSTEM_PROMPT),
+                Message(role=USER, content=
+                "Translate the word(s) '{}' in the context of the following sentence: '{}'.".format(chunk, sentence))]
+    response = exchange(messages)
+    return parse_response(response)
+
+
+def parse_response(response: str) -> list[Word]:
+    parsed = json.loads(response)
+    return [dacite.from_dict(data_class=Word, data=word) for word in parsed]
--- src/test_benchmark.py
+++ src/test_benchmark.py
@@ -1,31 +1,16 @@
 import logging
 import os
+import time
 from dataclasses import dataclass
 from unittest import TestCase
 
 from dacite import DaciteError
 
 from src.celebrities import get_celebrities_response
+from src.literal_translations import get_literal_translation
 from src.util.error import StructuralError
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-
-def read_celebrity_data():
-    data_file = "../test/celebrities.csv"
-    with open(data_file, "r") as f:
-        for line in f.readlines():
-            if not line.startswith("#"):
-                yield line.strip().split(";")
-
-
-def parse_bool(string: str) -> bool:
-    if string.lower() == "true":
-        return True
-    elif string.lower() == "false":
-        return False
-    else:
-        raise ValueError(f"Cannot parse {str} to boolean")
 
 
 @dataclass
@@ -78,3 +63,57 @@ class TestBenchmark(TestCase):
         print(f"Content accuracy: {100 - (content_error.counter / size * 100)}%")
         self.assertLessEqual(0, struct_error.counter)
         self.assertLessEqual(0, content_error.counter)
+
+    def test_benchmark_literal_translations(self):
+        struct_error = StructuralErrorCounter()
+        content_error = ContentErrorCounter()
+
+        size = 4
+
+        for sentence in read_literal_translation_data():
+            logging.info(f"Sentence: {sentence}")
+
+            # perform request, parse response
+            try:
+                response = get_literal_translation(sentence, 1)
+                self.assertEqual(len(response.words), len(sentence.split()))
+                logging.info(f"Response: {response}")
+            # catch errors, increment error counters
+            except StructuralError as se:
+                struct_error.increment()
+                logging.warning(f"Structural error: {se.response}")
+                continue
+            except AssertionError as ae:
+                content_error.increment()
+                logging.warning(f"Error with assertion: {ae} in response {response}")
+                continue
+
+        time.sleep(1)
+        print(f"Structural accuracy: {100 - (struct_error.counter / size * 100)}%")
+        print(f"Content accuracy: {100 - (content_error.counter / size * 100)}%")
+        self.assertLessEqual(0, struct_error.counter)
+        self.assertLessEqual(0, content_error.counter)
+
+
+def read_celebrity_data():
+    data_file = "../test/celebrities.csv"
+    with open(data_file, "r") as f:
+        for line in f.readlines():
+            if not line.startswith("#"):
+                yield line.strip().split(";")
+
+
+def read_literal_translation_data():
+    data_file = "../test/literal_translation.csv"
+    with open(data_file, "r") as f:
+        for line in f.readlines():
+            yield line.strip()
+
+
+def parse_bool(string: str) -> bool:
+    if string.lower() == "true":
+        return True
+    elif string.lower() == "false":
+        return False
+    else:
+        raise ValueError(f"Cannot parse {str} to boolean")
